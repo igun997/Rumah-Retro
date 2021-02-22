5d9c1aec02cc44abd299b03540b41c31ccfa29b5
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2021-02-22 17:59:44 +0700

    landing baru

--- resources/views/store/landing.blade.php
+++ resources/views/store/landing.blade.php
@@ -2,15 +2,98 @@
 @section("title","Selamat Datang Di Rumah Retro")
 @section("content")
 
-    <div class="popular-items section-padding30">
+    <div class="popular-items">
         <div class="container">
             <!-- Section tittle -->
             <div class="row justify-content-center">
-                <div class="col-xl-7 col-lg-8 col-md-10">
-                    <div class="section-tittle mb-40 text-center">
+                <div class="col-10">
+                    <div class="section-tittle text-center">
                         <img class="img-fluid" src="{{url("logo.png")}}" alt="">
+                        <h3>Welcome to Rumah Retro Production</h3>
                     </div>
+                    <hr>
+                    <p class="text-justify">
+                        Rumah Retro – Clothing Industri berdiri sejak bulan Juni 2003 dan merupakan industri yang dikelola oleh tenaga professional muda yang penuh kreativitas dan inovasi serta mempunyai semangat kerja yang tinggi, atas kepercayaan dari konsumen yang masih kerjasama dengan kami dari perusahaan, instansi, lembaga pendidikan, organisasi, dan komunitas. Kami terus menerus mengembangkan sumber daya manusia maupun alat-alat pendukung usaha
+                    </p>
+                    <br>
                 </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-8">
+                    <div class="section-tittle text-center">
+                        <h3 align="center">Mengapa Harus Rumah Retro Production</h3>
+                    </div>
+                    <hr>
+                    <center>
+                    <div class="col-6">
+                    <div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="vc_column-inner "><div class="wpb_wrapper">
+                        <div class="wpb_single_image wpb_content_element vc_align_center">
+                            <center>
+                            <figure class="wpb_wrapper vc_figure">
+                                <div class="vc_single_image-wrapper vc_box_outline  vc_box_border_orange"><img class="vc_single_image-img " src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_80,h_80/https://konveksirumahretrobandung.id/wp-content/uploads/2018/12/SDM-80x80.png" width="80" height="80" alt="SDM" title="SDM"></div>
+                            </figure>
+                            </center>
+                        </div>
+                    
+                        <div class="wpb_text_column wpb_content_element ">
+                            <div class="wpb_wrapper">
+                                <p style="text-align: center;"><span style="font-size: 14pt; color: #000000; font-family: 'book antiqua', palatino, serif;">Ditangani oleh tenaga yang profesional dalam bidangnya, berkecimpung di bidangnya kurang lebih selama 15 tahun. Maka tidak diragukan lagi jika pesananmu akan selalu maksimal dan berkualitas</span></p>
+                            </div>
+                        </div>
+                    </div></div></div><div class="wpb_column vc_column_container vc_col-sm-3"><div class="vc_column-inner "><div class="wpb_wrapper">
+                        <div class="wpb_single_image wpb_content_element vc_align_center">
+                            <center>
+                            <figure class="wpb_wrapper vc_figure">
+                                <div class="vc_single_image-wrapper vc_box_outline  vc_box_border_orange"><img class="vc_single_image-img " src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_80,h_80/https://konveksirumahretrobandung.id/wp-content/uploads/2018/12/komputer-80x80.png" width="80" height="80" alt="komputer" title="komputer"></div>
+                            </figure>
+                        </center>
+                        </div>
+                    
+                        <div class="wpb_text_column wpb_content_element ">
+                            <div class="wpb_wrapper">
+                                <p style="text-align: center;"><span style="font-size: 14pt; color: #000000; font-family: 'book antiqua', palatino, serif;">Rumah Retro Production sudah mempunyai mesin produksi sendiri seperti halnya mesin bordir, mesin sublime printing. Jadi semua orderanmu akan ditangani langsung tanpa melalui pihak ketiga</span></p>
+                    
+                            </div>
+                        </div>
+                    </div></div></div><div class="wpb_column vc_column_container vc_col-sm-3"><div class="vc_column-inner "><div class="wpb_wrapper">
+                        <div class="wpb_single_image wpb_content_element vc_align_center">
+                        <center>
+                            
+                            <figure class="wpb_wrapper vc_figure">
+                                <div class="vc_single_image-wrapper vc_box_outline  vc_box_border_orange"><img class="vc_single_image-img " src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_80,h_80/https://konveksirumahretrobandung.id/wp-content/uploads/2018/12/HARGA-80x80.png" width="80" height="80" alt="HARGA" title="HARGA"></div>
+                            </figure>
+                        </center>
+
+                        </div>
+                    
+                        <div class="wpb_text_column wpb_content_element ">
+                            <div class="wpb_wrapper">
+                                <p style="text-align: center;"><span style="font-size: 14pt; color: #000000; font-family: 'book antiqua', palatino, serif;">Memberikan harga yang sesuai dengan kualitas bahan, sablon, dll.Tentu yang kami tawarkan merupakan harga yang paling murah diantara yang lain </span></p>
+                    
+                            </div>
+                        </div>
+                    </div></div></div><div class="wpb_column vc_column_container vc_col-sm-3"><div class="vc_column-inner "><div class="wpb_wrapper">
+                        <div class="wpb_single_image wpb_content_element vc_align_center">
+                        <center>
+                            
+                            <figure class="wpb_wrapper vc_figure">
+                                <div class="vc_single_image-wrapper vc_box_outline  vc_box_border_orange"><img class="vc_single_image-img " src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_80,h_80/https://konveksirumahretrobandung.id/wp-content/uploads/2018/12/Pemesanan-80x80.png" width="80" height="80" alt="Pemesanan" title="Pemesanan"></div>
+                            </figure>
+                        </center>
+
+                        </div>
+                    
+                        <div class="wpb_text_column wpb_content_element ">
+                            <div class="wpb_wrapper">
+                                <p style="text-align: center;"><span style="font-size: 14pt; color: #000000; font-family: 'book antiqua', palatino, serif;">Rumah Retro Production melayani pesanan dalam skala kecil maupun skala besar. Tentu kami tidak membedakan dalam proses pengerjaan, baik skala kecil maupun besar akan di selesaikan sesuai dengan kesepakatan.</span></p>
+                    
+                            </div>
+                        </div>
+                    </div></div></div>
+                    </div>
+                    </center>
+                </div>
+            </div>
             </div>
             <div class="row">
                 @foreach($products as $key => $product)
@@ -34,45 +117,8 @@
                 </div>
                 @endforeach
             </div>
-            <!-- Button -->
-{{--            <div class="row justify-content-center">--}}
-{{--                <div class="room-btn pt-70">--}}
-{{--                    <a href="catagori.html" class="btn view-btn1">Lihat Produk Lainnya </a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
         </div>
     </div>
-
-    <div class="shop-method-area">
-        <div class="container">
-            <div class="method-wrapper">
-                <div class="row d-flex justify-content-between">
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="single-method mb-40">
-                            <i class="ti-package"></i>
-                            <h6>Free Shipping Method</h6>
-                            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="single-method mb-40">
-                            <i class="ti-unlock"></i>
-                            <h6>Secure Payment System</h6>
-                            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="single-method mb-40">
-                            <i class="ti-reload"></i>
-                            <h6>Secure Payment System</h6>
-                            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Shop Method End-->
 @endsection
 @section("js")
     <script>
